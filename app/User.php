0fb24c8a41e50cc9bdc3d37ed3f7a36f701cde4a
Carmen Luna <carmen.oprea@hotmail.com>
2018-05-23 15:39:02 +0200

    Added migration and changed Role and User relationship

--- app/User.php
+++ app/User.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    public function role(){
+        return $this->belongsTo("App\User");
+    }
     use Notifiable;
 
     /**
