c4082339c4d8ca045215c06f678627d39c6c34f6
Carmen Luna <carmen.oprea@hotmail.com>
2018-05-24 11:12:18 +0200

    created jobs table

---
+++ database/migrations/2018_05_23_140042_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("posted_by_id");
+            $table->integer("job-type_id");
+            $table->integer("company_id");
+            $table->string("title");
+            $table->mediumText("job-description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
