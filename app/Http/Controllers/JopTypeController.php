34e8644ec24147c1e28eccd23cd2799aa74d75a6
Carmen Luna <carmen.oprea@hotmail.com>
2018-05-27 20:22:41 +0200

    created jobs and job types table and established their relationship

---
+++ app/Http/Controllers/JopTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\JopType;
+use Illuminate\Http\Request;
+
+class JopTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\JopType  $jopType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JopType $jopType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\JopType  $jopType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JopType $jopType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\JopType  $jopType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JopType $jopType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\JopType  $jopType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JopType $jopType)
+    {
+        //
+    }
+}
---
+++ app/JopType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JopType extends Model
+{
+    public function job(){
+        return $this->hasMany("App\Jobs");
+    }
+
+}
