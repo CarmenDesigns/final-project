ff0ea702ff7b5f85f828fe326c3029e55938d7bf
Carmen Luna <carmen.oprea@hotmail.com>
2018-06-13 21:26:40 +0200

    users search bar

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,23 +9,35 @@
 <h1>Users</h1>
 
 <a href="{{ url('users/create') }}">Add new<i class="fas fa-user-plus"></i></a>
+    <div class="search">
+        <a> <input type="text" class="search-field" id="link_box" placeholder="Search for names.."></a>
+        <button id="search-button" value="search" onclick="window.location = document.getElementById('link_box').value;"><i class="fas fa-search"></i></button>
+        {{--<input type="button" id="search-button" value="search" onclick="window.location = document.getElementById('link_box').value;">--}}
+        <div id="results"></div>
+    </div>
 </div>
 @foreach($users as $user)
 
 <div class="container-users">
 
-        <h3>{{$user->name}}</h3>
 
+           <h3>{{$user->name}}</h3>
     <form method="post" action="/users/{{$user->id}}">
         @csrf
         <div class="input">
-        <input name="_method" type="hidden" value="DELETE">
-        <input type="submit" value="Delete">
-        <a href="{{ url("users/$user->id/edit") }}">Edit</a><br><br>
+            <input name="_method" type="hidden" value="DELETE">
+            <input type="submit" value="Delete">
+            <a href="{{ url("users/$user->id/edit") }}">Edit</a><br><br>
         </div>
     </form>
 </div>
 
+<script type="text/javascript">
+    document.getElementById("search-button").addEventListener("click", function(){
+        window.location = document.getElementById('link_box').value;
+    });
+
+</script>
 
 @endforeach
 
